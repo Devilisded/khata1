45e722678d8b7ec56df34258cefde1032d8c0104
Devilisded <142964931+Devilisded@users.noreply.github.com>
2023-12-14 17:39:07 +0530

    characterPicSrc

--- src/components/staff/staffSelect/StaffSelect.jsx
+++ src/components/staff/staffSelect/StaffSelect.jsx
@@ -1,9 +1,9 @@
-import React from "react";
+import character from "../../../assets/character.png";
 
 const StaffSelect = () => {
   return (
     <div className="h-[100vh - 87px] flex flex-col justify-center items-center w-full bg-slate-100 gap-5">
-      <img src="../src/assets/character.png" alt="" className=" w-96" />
+      <img src={character} alt="" className=" w-96" />
       <p className="font-semibold text-xl">No Staff Selected</p>
     </div>
   );
